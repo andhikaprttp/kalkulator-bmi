e50fd8958014dd2db1e84d4d4990678fed1b9b02
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-30 12:23:24 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function hitungBMI() {
             let berat = parseFloat(document.getElementById("berat").value);
+ 
             let tinggi = parseFloat(document.getElementById("tinggi").value            // Konversi tinggi dari cm ke meter
             let tinggidiMeter = tinggi / 100;
 
