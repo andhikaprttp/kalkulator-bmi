507eaeb6a82ff4e373b86a957383c3d87d32f461
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-30 12:24:20 +0700

    update script.js

--- script.js
+++ script.js
@@ -11,5 +11,15 @@ function hitungBMI() {
             let resultElement = document.getElementById("result");
             resultElement.innerHTML = "BMI Anda: " + bmi.toFixed(2);
 
-        
+            // Menentukan kategori BMI
+            if (bmi < 18.5) {
+                resultElement.innerHTML += " (Kurus)";
+            } else if (bmi >= 18.5 && bmi < 25) {
+                resultElement.innerHTML += " (Normal)";
+            } else if (bmi >= 25 && bmi < 30) {
+                resultElement.innerHTML += " (Gemuk)";
+            } else {
+                resultElement.innerHTML += " (Obesitas)";
+            }
+        }
             
